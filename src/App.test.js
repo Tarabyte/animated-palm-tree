e2b1484b9c8d0940ca1a0ed86ba1ffa33ac51253
Yury Tarabanko <tarabanko@mirapolis.ru>
2019-12-09 12:35:08 +0300

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('Application', () => {
+  it('renders without crashing 2', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
+
+
